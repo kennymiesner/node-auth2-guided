8367fd95862452b142e98458dc28458ebaaac625
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-14 19:10:53 +0200

    wip

--- api/auth/restricted-middleware.js
+++ api/auth/restricted-middleware.js
@@ -4,6 +4,12 @@ const { JWT_SECRET } = require('../../config')
 module.exports = (req, res, next) => {
   const token = req.headers.authorization
   if (!token) return next({
-    
+    status: 401, message: 'we wants token!!!'
   })
+
+  jwt.verify(
+    token,
+    JWT_SECRET,
+    
+  )
 };

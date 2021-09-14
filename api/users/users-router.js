75d5df0c38837e3b843a399c445dce571c0593b7
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-14 19:22:23 +0200

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-
+const checkRole = require('../auth/check-role-middleware')
 const Users = require("./users-model.js");
 const restricted = require("../auth/restricted-middleware.js");
 
-router.get("/", restricted, checkAdmin, (req, res, next) => {
+router.get("/", restricted, checkRole, (req, res, next) => {
   Users.find()
     .then(users => {
       res.json(users);

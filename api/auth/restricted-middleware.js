00ad14474107cd27c5b20864e86a0d40d25557eb
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-09-14 19:04:45 +0200

    wip

--- api/auth/restricted-middleware.js
+++ api/auth/restricted-middleware.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../../config')
+
 module.exports = (req, res, next) => {
   // add code here to verify users are logged in
   next();
